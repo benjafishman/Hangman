f9dab06538369d68f9dd9714fedd84d5b52bf119
Ben Fishman <benjafishman@gmail.com>
2015-09-06 16:06:11 -0400

    - trying to fix migration conflict

---
+++ hangman/migrations/0009_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('hangman', '0008_auto_20150906_1327'),
+        ('hangman', '0008_hangman_word_attempt'),
+    ]
+
+    operations = [
+    ]
