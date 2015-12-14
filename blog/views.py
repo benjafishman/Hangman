338aee1546c926db31810cb6aad9c061874753f6
Ben Fishman <benjafishman@gmail.com>
2015-12-13 23:57:41 -0500

    - bunch of template view updates (got count for parsha question victory)

--- blog/views.py
+++ blog/views.py
@@ -3,17 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.shortcuts import render_to_response, get_object_or_404
-from blog.models import GemaraPost, Category, ParshaPost, Chumash,\
-Parsha, ParshaQuestion
+from django.db.models import Count
+from blog.models import GemaraPost, Category, ParshaPost, Chumash, \
+    Parsha, ParshaQuestion
 from django.http import HttpResponse
 
 '''
 The index view simply lists the categories one could click to look at
 FOR NOW
 '''
-def index(request):
 
-    #posts = Post.objects.filter(published=True)
+
+def index(request):
+    # posts = Post.objects.filter(published=True)
     categories = Category.objects.all()
 
     shteig_header = "home"
@@ -21,17 +23,20 @@ def index(request):
     # get most recent 5 parsha posts (posts with section == parsha)
     gemara_posts = GemaraPost.objects.all()
     #halacha_posts = Post.objects.filter(section__title__startswith='Halacha')
-    return render(request, 'blog/index.html', {'categories': categories, 'gemara_posts': gemara_posts, 'subheader': shteig_header})
+    return render(request, 'blog/index.html',
+                  {'categories': categories, 'gemara_posts': gemara_posts, 'subheader': shteig_header})
+
 
 def about(request):
-    return render(request,'blog/about.html', context)
+    return render(request, 'blog/about.html', context)
+
 
 def post(request, slug):
     print("post view")
     print("request: " + str(request))
     print("slug: " + slug)
     # get the Post object
-    #post = get_object_or_404(GemaraPost, slug=slug)
+    # post = get_object_or_404(GemaraPost, slug=slug)
     post = get_object_or_404(ParshaPost, slug=slug)
     related_posts = GemaraPost.objects.all().exclude(pk=post.pk)
 
@@ -39,7 +44,9 @@ def post(request, slug):
 
     subheader = post.get_title()
     # now return the rendered template
-    return render(request, 'blog/'+template_name, {'post': post, 'subheader': subheader, 'related_posts':related_posts})
+    return render(request, 'blog/' + template_name,
+                  {'post': post, 'subheader': subheader, 'related_posts': related_posts})
+
 
 def category_post_list(request, category):
     print("category post view")
@@ -47,45 +54,73 @@ def category_post_list(request, category):
     posts = Post.objects.filter(categories__title__startswith=category)
     return render(request, 'blog/view_category.html', {'posts': posts})
 
+
 def chumash_parsha_listing(request):
-    #genesis = Parsha.objects.filter(chumash__title__startswith='Genesis').order_by('order')
+    # genesis = Parsha.objects.filter(chumash__title__startswith='Genesis').order_by('order')
     chumash = {'Genesis':
-                   ['Bereishis','Noach','Lech Lecha', 'Vayeira','Chayei Sara', 'Toldos', 'Vayeitzei',\
-                    'Vayishlach','Vayeishev','Mikeitz','Vayigash','Vayechi'],
-               'Exodus':[],
-               'Leviticus':[],
-               'Numbers':[],
-               'Deuteronomy':[]
+                   ['Bereishis', 'Noach', 'Lech Lecha', 'Vayeira', 'Chayei Sara', 'Toldos', 'Vayeitzei', \
+                    'Vayishlach', 'Vayeishev', 'Mikeitz', 'Vayigash', 'Vayechi'],
+               'Exodus': [],
+               'Leviticus': [],
+               'Numbers': [],
+               'Deuteronomy': ['eikev']
     }
-    return render(request, 'blog/chumash-parsha-list.html', {'books':chumash})
+    return render(request, 'blog/chumash-parsha-list.html', {'books': chumash})
+
 
 def sefer_parsha_listing(request, sefer):
-
     chumash = {'Genesis':
-                   ['Bereishis','Noach','Lech Lecha', 'Vayeira','Chayei Sara', 'Toldos', 'Vayeitzei',\
-                    'Vayishlach','Vayeishev','Mikeitz','Vayigash','Vayechi'],
-               'Exodus':[],
-               'Leviticus':[],
-               'Numbers':[],
-               'Deuteronomy':[]
+                   ['Bereishis', 'Noach', 'Lech Lecha', 'Vayeira', 'Chayei Sara', 'Toldos', 'Vayeitzei', \
+                    'Vayishlach', 'Vayeishev', 'Miketz', 'Vayigash', 'Vayechi'],
+               'Exodus': [],
+               'Leviticus': [],
+               'Numbers': [],
+               'Deuteronomy': ['deuteronomy', 'eikev']
     }
-    parshas = []
-    if sefer in chumash:
-        parshas = chumash[sefer]
-    print(parshas)
-    return render(request, 'blog/sefer_parsha_listing.html', {'parshas':parshas, 'sefer':sefer})
+    parshas_with_questions = list(ParshaQuestion.objects.filter(parsha__chumash__title=sefer).values('parsha__eng_name',
+                                                                                                'parsha__order').annotate(
+        total=Count('parsha__eng_name')).order_by('total'))
+    # have to add the rest of the parsha with 0 and their orders
+    i = 0
+    for parsha in chumash[sefer]:
+        # if current parsha is in not returned add it with its totsl set to 0 and order
+        if not any(d['parsha__eng_name'] == parsha.lower() for d in parshas_with_questions):
+            #print(parsha)
+            parshas_with_questions.append( {'parsha__eng_name':parsha,'parsha__order':i,'total':0})
+        i+=1
+    return render(request, 'blog/sefer_parsha_listing.html', {'parshas': parshas_with_questions, 'sefer': sefer})
+
 
 def parsha_questions_list(request, parsha_name):
     # get all questions related to parsha id returned in descending order by date creaed
 
     # convert query string to lower case
-    parsha_name = parsha_name.lower()
+    title = parsha_name.title()
     questions = ParshaQuestion.objects.filter(parsha__eng_name=parsha_name).order_by('created')
-    return render(request, 'blog/parsha_questions_list.html', {'questions':questions})
+
+    return render(request, 'blog/parsha_questions_list.html', {'questions': questions, 'title':title})
+
 
 def parsha_question_detail(request, question_id):
     question = get_object_or_404(ParshaQuestion, pk=question_id)
 
     # query for other question in selected parsha but exclud current question
     related_question = ParshaQuestion.objects.filter(parsha__eng_name=question.parsha.eng_name).exclude(pk=question.pk)
-    return render(request, 'blog/parsha_question_detail.html',{'question':question,'related_questions': related_question})
+    return render(request, 'blog/parsha_question_detail.html',
+                  {'question': question, 'related_questions': related_question})
+
+def parsha_to_chumash(parsha):
+    chumash = {'Genesis':
+                   ['Bereishis', 'Noach', 'Lech Lecha', 'Vayeira', 'Chayei Sara', 'Toldos', 'Vayeitzei', \
+                    'Vayishlach', 'Vayeishev', 'Miketz', 'Vayigash', 'Vayechi'],
+               'Exodus': [],
+               'Leviticus': [],
+               'Numbers': [],
+               'Deuteronomy': ['deuteronomy', 'eikev']
+    }
+
+    for sefer in chumash:
+        if parsha.title() in chumash[sefer]:
+            return sefer
+
+    return False
