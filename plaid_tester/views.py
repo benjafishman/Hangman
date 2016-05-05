f41a11a4efe86f450cd1683c3eca34a5f7accf5e
Ben Fishman <benjafishman@gmail.com>
2016-05-04 21:39:12 -0600

    - updating for live account

--- plaid_tester/views.py
+++ plaid_tester/views.py
@@ -8,28 +8,45 @@ from django.core.urlresolvers import reverse
 from plaid import Client
 from plaid import errors as plaid_errors
 from django.conf import settings
+import sys
+
 
 def index(request):
-
     return render(request, 'plaid_tester/index.html')
 
+
 def data_dump(request):
-    accounts=None
+    accounts = None
     transactions = None
     if request.method == 'POST':
 
-        #print(request.POST)
+        # print(request.POST)
         client = Client(client_id=settings.PLAID_CLIENT_ID, secret=settings.PLAID_SECRET)
 
-        response = client.exchange_token(request.POST['public_token'])
+        try:
+            response = client.exchange_token(request.POST['public_token'])
+            print(response.json())
+        except:
+            print(sys.exc_info()[0])
+
+
+        try:
+            response = client.upgrade('connect')
+            print(response.json())
+        except:
+            print(sys.exc_info()[0])
         # client.access_token should now be populated with a
         # valid access_token;  we can make authenticated requests
 
-        print(response.json())
 
-        accounts = client.auth_get().json()['accounts']
-
+        try:
+            accounts = client.auth_get().json()['accounts']
+        except:
+            print(sys.exc_info()[0])
         # client.connect_get retrieves account data as  well!!
-        transactions = client.connect_get().json()['transactions']
+        try:
+            transactions = client.connect_get().json()['transactions']
+        except:
+            print(sys.exc_info()[0])
 
-    return render(request, 'plaid_tester/data_dump.html',{'accounts': accounts, 'transactions':transactions})
+    return render(request, 'plaid_tester/data_dump.html', {'accounts': accounts, 'transactions': transactions})
